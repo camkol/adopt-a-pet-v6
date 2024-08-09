da5e37fbc34b4051cf3576ae5c3f4df6166d0d1d
camkol <koliyus3@hotmail.com>
2024-08-09 13:01:20 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import HomePage from './pages/home';
-import SearchPage from './pages/search';
-import PetDetailsPage from './pages/detail';
-import PetDetailsNotFound from './pages/petDetailsNotFound';
-import Root from './components/root';
+import HomePage from "./pages/home";
+import SearchPage from "./pages/search";
+import PetDetailsPage from "./pages/detail";
+import PetDetailsNotFound from "./pages/petDetailsNotFound";
+import Root from "./components/root";
 
 // Add react-router-dom imports
+import { RouterProvider } from "react-router-dom";
 
 // create router with JSX Route elements
 const appRouter = "REPLACE ME";
@@ -12,7 +13,7 @@ const appRouter = "REPLACE ME";
 function App() {
   return (
     // replace below with a Router Provider
-    <p>REPLACE ME</p>
+    <RouterProvider router={appRouter} />
   );
 }
 
