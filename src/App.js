167853a8ecd7ef379acd3e0632a7c0aa7722446b
camkol <koliyus3@hotmail.com>
2024-08-11 10:25:28 +0200

    may need fix

--- src/App.js
+++ src/App.js
@@ -16,7 +16,9 @@ import {
 const appRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
-      <Route index element={<HomePage />} />
+      <Route index element={<HomePage />}>
+        <Route path=":type" element={<HomePage />} />
+      </Route>
     </Route>
   )
 );
